f2444dc700dfd9b6b35af35455240a4c8da5f378
Anderson <anderson.s.reis6@ba.estudante.senai.br>
2023-11-22 15:01:46 -0300

    removing debugs

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -8,13 +8,7 @@ import netflix from '../../photos/netflix.png'
 import netflix_main from '../../photos/netflix-main.png'
 
 export function Portifolio({user}){
-    const [data, setData] = useState([])
-    const [filteredData, setFilteredData] = useState([])
     const [repoDetails, setRepoDedails] = useState([])
-    const [responses, setResponses] = useState([])
-
-   
-    
     useEffect(()=> { 
         
         const token = "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"
@@ -33,12 +27,7 @@ export function Portifolio({user}){
             
             const allRepos = await fetch(`https://api.github.com/users/${user}/repos`, options)
             .then(data => data.json())
-        
-            console.log(allRepos)
-            
             const filteredByApi = allRepos.filter(repo => repo.name.includes("API") || repo.name.includes('api') || repo.name.includes('frontend') || repo.name.includes("react"))
-            console.log(filteredByApi[0].updated_at)
-
             setRepoDedails(filteredByApi) 
         }   
 
@@ -60,7 +49,6 @@ export function Portifolio({user}){
                 const formatedDate = dates.replaceAll('-', '/')
                 const finalDate = formatedDate.slice(0, 10)
                 const formatedTime = formatedDate.slice(11, formatedDate.length -1)
-                console.log(finalDate)
                 return <ProjectCard 
                         key = {String(index)}
                         title = {repo.name} 
