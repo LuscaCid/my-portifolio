fcedd05e3e607b66684283096b71bfaea6c0ec2e
Anderson <anderson.s.reis6@ba.estudante.senai.br>
2023-11-22 15:09:25 -0300

    finishing

--- src/components/stackCard/style.js
+++ src/components/stackCard/style.js
@@ -19,19 +19,21 @@ export const Container = styled.div`
     box-shadow: ${({lang})=> {
         switch(lang){
             case "javascript" : 
-                return `0 1rem 2rem -1rem rgb(255,255,0,0.4)`
+                return `0 1rem 1.7rem -1rem rgb(255,255,0,0.8)`
+            case "html" :
+                return "0 1rem 1.7rem -1rem #f0650980"
             case "react" : 
-                return "0 1rem 2rem -1rem #61DBFB50"
+                return "0 1rem 1.7rem -1rem #61DBFB80"
             case "git" : 
-                return  "0 1rem 2rem -1rem #f1502f50"   
+                return  "0 1rem 1.7rem -1rem #f1502f80"   
             case "node" :
-                return "0 1rem 2rem -1rem #78a06350"
+                return "0 1rem 1.7rem -1rem #78a06380"
             case "github" : 
-                return "0 1rem 2rem -1rem #ffffff50"
+                return "0 1rem 1.7rem -1rem #ffffff80"
             case "sqlite" : 
-                return "0 1rem 2rem -1rem #61DBFB50"
+                return "0 1rem 1.7rem -1rem #61DBFB80"
             case "npm" :
-                return "0 1rem 2rem -1rem #f1502f50"
+                return "0 1rem 1.7rem -1rem #f1502f80"
             case "css" :
                 return "0 1rem 2rem -1rem #61DBFc50"
         }
