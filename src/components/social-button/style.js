4646b0c2d375630190e2a02a507361a2a5fc8b48
Anderson <anderson.s.reis6@ba.estudante.senai.br>
2023-11-22 15:15:40 -0300

    finally

--- src/components/social-button/style.js
+++ src/components/social-button/style.js
@@ -10,7 +10,7 @@ export const Container = styled.div`
     >a{
         border: 1px solid #414141;
         border-radius: .3rem;
-        padding: .3rem 1rem;
+        padding: .3rem .8rem;
         background: ${({theme}) => theme.colors.background};
         display: flex;
         align-items: center;
@@ -19,5 +19,11 @@ export const Container = styled.div`
         font-size: 1.8rem;
         gap: 1rem;
     }
-
+    @media(max-width: 41em){
+        
+        >a{
+            padding: 0.2rem .7rem;
+            width: 4.3rem;
+        }
+    } 
 `
